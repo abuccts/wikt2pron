29023ef4e0e2a614aaabad0ba9ae0cf726bd881a
abuccts <abuccts@gmail.com>
2017-07-26 17:42:08 +0800

    add IPA_to_CMUBET

--- pywiktionary/IPA/IPA.py
+++ pywiktionary/IPA/IPA.py
@@ -6,7 +6,10 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import regex as re
+
 from .data import XSAMPA
+from .data import CMUBET
 
 
 # X-SAMPA symbol set
@@ -23,6 +26,42 @@ for XSAMPA_symbol, IPA_data in m_XSAMPA.items():
     if "with_descender" in IPA_data.keys():
         with_descender = IPA_data["with_descender"]
         i2x_lookup[with_descender] = XSAMPA_symbol
+
+# CMUBET symbol set
+m_CMUBET = CMUBET.data
+
+# IPA <-> CMUBET lookup tables
+# IPA symbols not in CMUBET are included from Arpabet
+# Refer to https://en.wikipedia.org/wiki/Arpabet for details
+i2c_lookup = {
+    "ɪ": "IH",
+    "ə": "AX",
+    "aɪ": "AY",
+    "aʊ": "AW",
+    "ɝ": "ER",
+    "ɚ": "AXR",
+    "dʒ": "JH",
+    "m̩": "EM",
+    "n̩": "EN",
+    "ŋ̍": "ENG",
+    "ɫ": "L",
+    "ɫ̩": "EL",
+    "r": "R",
+    "ɾ": "DX",
+    "ɾ̃": "NX",
+    "ʔ": "Q",
+    # from http://courses.csail.mit.edu/6.345//notes/IPA/
+    "e": "EY",
+    "ɦ": "HV",
+    "o": "OW",
+    "ɨ": "IX",
+    # add to nearest phoneme
+    "a": "AA",
+    "ɜ": "ER",
+    "ɒ": "O",
+}
+for CMUBET_symbol, IPA_symbol in m_CMUBET.items():
+    i2c_lookup[IPA_symbol] = CMUBET_symbol
 
 
 def IPA_to_XSAMPA(text):
@@ -57,7 +96,7 @@ def IPA_to_XSAMPA(text):
     >>> XSAMPA_text
     "/t__SeInd__Z/"
     """
-    text = text.replace('ːː', ':')
+    text = re.sub("ːː", ":", text)
     text += " "
     XSAMPA_lst = []
     i = 0
@@ -71,3 +110,39 @@ def IPA_to_XSAMPA(text):
             XSAMPA_lst.append(text[i])
         i += 1
     return "".join(XSAMPA_lst)
+
+
+def IPA_to_CMUBET(text):
+    """Convert IPA to CMUBET for US English.
+
+    Use `IPA`_ and symbol set used in Wiktionary and
+    `CMUBET`_ symbol set used in CMUDict.
+
+    .. _IPA: https://en.wiktionary.org/wiki/Module:IPA/data/symbols
+    .. _CMUBET: https://cmusphinx.github.io/wiki/cmubet/
+
+    Parameters
+    ----------
+    text : string
+        String of IPA text parsed from Wiktionary.
+
+    Returns
+    -------
+    string
+        Converted CMUBET text.
+    """
+    text = re.sub("ːː", ":", text)
+    text = text.lstrip("/[").rstrip("]/")
+    text += " "
+    CMUBET_lst = []
+    i = 0
+    while i < len(text) - 1:
+        if text[i:i+2] in i2c_lookup.keys():
+            CMUBET_lst.append(i2c_lookup[text[i:i+2]])
+            i += 1
+        elif text[i] in i2c_lookup.keys():
+            CMUBET_lst.append(i2c_lookup[text[i]])
+        else:
+            continue
+        i += 1
+    return " ".join(CMUBET_lst)
---
+++ pywiktionary/IPA/data/CMUBET.py
@@ -0,0 +1,52 @@
+"""CMUBET symbol set.
+Available from https://cmusphinx.github.io/wiki/cmubet/
+"""
+
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
+# CMUBET symbols
+data = {
+    "AA": "ɑ",
+    "AE": "æ",
+    "AH": "ʌ",
+    "AO": "ɔ",
+    "AW": "ɑʊ",
+    "AY": "ɑɪ",
+    "B": "b",
+    "CH": "ʧ",
+    "D": "d",
+    "DH": "ð",
+    "EH": "ɛ",
+    "ER": "ɜɹ",
+    "EY": "eɪ",
+    "F": "f",
+    "G": "ɡ",
+    "HH": "h",
+    "IH": "i",
+    "IY": "ɪː",
+    "JH": "ʤ",
+    "K": "k",
+    "L": "l",
+    "M": "m",
+    "N": "n",
+    "NG": "ŋ",
+    "OW": "oʊ",
+    "OY": "ɔɪ",
+    "P": "p",
+    "R": "ɹ",
+    "S": "s",
+    "SH": "ʃ",
+    "SIL": ".",
+    "T": "t",
+    "TH": "θ",
+    "UH": "ʊ",
+    "UW": "u",
+    "V": "v",
+    "W": "w",
+    "Y": "j",
+    "Z": "z",
+    "ZH": "ʒ",
+}
